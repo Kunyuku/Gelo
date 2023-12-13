d75e0af757000010e8e4efb4f85d96941610b554
Kunyuku <136298955+Kunyuku@users.noreply.github.com>
2023-12-13 11:47:26 +0700

    Update Gila.sh

--- Gila.sh
+++ Gila.sh
@@ -35,6 +35,10 @@ bash Firewall.sh
 sudo cp fw_v1.x_generic /etc/init.d/fw_v1.x_generic
 sudo update-rc.d fw_v1.x_generic defaults
 sudo /etc/init.d/fw_v1.x_generic start
+  sleep 5
+git clone https://github.com/apoorvar5/InfrastructureAsACodeForAWS.git
+cd InfrastructureAsACodeForAWS
+bash installation.sh
 
   sleep 5
   
